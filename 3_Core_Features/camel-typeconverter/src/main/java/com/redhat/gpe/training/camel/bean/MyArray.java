c1e1199b840c7a09f9b0860b2b7a0e706514f7de
Charles Moulliard <ch007m@gmail.com>
2014-12-12 12:47:50 +0100

    Remove from the master the files of the branches

--- 3_Core_Features/camel-typeconverter/src/main/java/com/redhat/gpe/training/camel/bean/MyArray.java
+++
@@ -1,8 +0,0 @@
-package com.redhat.gpe.training.camel.bean;
-
-import java.util.ArrayList;
-
-/*
-    A simple Bean extending ArrayList
- */
-public class MyArray extends ArrayList<String> { }
--- 5_Components/camel-sql/src/main/resources/sql/db-schema.sql
+++
@@ -1,9 +0,0 @@
-create table projects (
-  id integer primary key,
-  project varchar(50),
-  license varchar(5)
-);
-insert into projects values (1, 'Apache Camel', 'ASF');
-insert into projects values (2, 'Apache ActiveMQ', 'ASF');
-insert into projects values (3, 'Apache Karaf', 'ASF');
-insert into projects values (4, 'JBoss Fabric8', 'ASF');
