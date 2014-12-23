cb6ba214c2bdebce3f642762de485a6b2ed9b613
Charles Moulliard <ch007m@gmail.com>
2014-12-23 17:36:39 +0100

    Remove code for the students

--- 7_Advanced_features/camel-security/src/main/java/com/redhat/gpe/training/camel/JettySecuredRoute.java
+++ 7_Advanced_features/camel-security/src/main/java/com/redhat/gpe/training/camel/JettySecuredRoute.java
@@ -8,7 +8,7 @@ public class JettySecuredRoute extends RouteBuilder {
     public static void main(String[] args) throws Exception {
         Main main = new Main();
         main.addRouteBuilder(new JettySecuredRoute());
-        main.bind("myAuthHandler",MySecurityHandler.generate());
+        // Bind the SecurityHandler with the key myAuthHandler
         main.enableHangupSupport();
         main.start();
     }
@@ -16,8 +16,7 @@ public class JettySecuredRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        from("jetty://http://localhost:9191/demo?handlers=myAuthHandler")
-                .transform(constant("<html><body><p>Bye World</p></body></html>"));
+    // Add the Jetty Secured Route
 
 
     }
