88eaeb4119723736aa7070a9729959e50713153e
Charles Moulliard <ch007m@gmail.com>
2015-01-13 17:31:28 +0100

    Remove import statements of Spring

--- 8_Modularity/osgi-plain-jar/src/main/java/com/redhat/gpe/training/bean/HelloBean.java
+++ 8_Modularity/osgi-plain-jar/src/main/java/com/redhat/gpe/training/bean/HelloBean.java
@@ -2,8 +2,6 @@ package com.redhat.gpe.training.bean;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
 /**
  * A HelloBean containing a string property.
