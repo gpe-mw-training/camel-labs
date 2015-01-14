a95e383ad2134ad1d0efd7f1356ab023dbb719ab
Charles Moulliard <ch007m@gmail.com>
2015-01-14 12:29:33 +0100

    Clean plain-jar code

--- 8_Modularity/osgi-plain-jar/src/main/java/com/redhat/gpe/training/bean/HelloBean.java
+++ 8_Modularity/osgi-plain-jar/src/main/java/com/redhat/gpe/training/bean/HelloBean.java
@@ -11,28 +11,19 @@ public class HelloBean {
     // LOG
     private static final Logger LOG = LoggerFactory.getLogger(HelloBean.class);
 
-    // Property
-    private String message;
+    // Message Property
 
     /**
      * Method for setting the above property
      */
-    public void setMessage(String message) {
-        this.message = message;
-    }
 
     /**
      * Method for initializing the bean and its required resources.
      */
-    public void init() throws Exception {
-        LOG.info("Initializing hello bean with message = '" + message + "'");
-    }
 
     /**
      * Method for shutting down the bean and its open resources.
      */
-    public void destroy() throws Exception {
-        LOG.info("Destroying hello bean with message = '" + message + "'");
-    }
+
 
 }
