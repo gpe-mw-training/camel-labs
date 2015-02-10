f16564f378b5e69f3fec5869ecdb7218c83631ad
Charles Moulliard <ch007m@gmail.com>
2015-02-10 09:12:34 +0100

    Post correct curl request

--- 9_Fabric/fabric-camel-rest/src/main/java/com/redhat/gpe/training/internal/RestfullClient.java
+++ 9_Fabric/fabric-camel-rest/src/main/java/com/redhat/gpe/training/internal/RestfullClient.java
@@ -13,13 +13,17 @@ public class RestfullClient {
     public static void main(String[] args) throws Exception {
 
         // Get a Customer
-        getCustomer2();
+        getCustomer();
 
         // Post
-        // postCustomer();
+        postCustomer();
 
     }
 
+    /*
+      DOES NOT WORK HTTPie req : echo {"Customer":{"id":"222","name":"charles"}} | http POST http://localhost:9090/rest/customerservice/customers
+      curl -i -H "Content-Type: application/json" -X POST -d '{"Customer":{"id":"666","name":"Charles"}}' http://localhost:9090/rest/customerservice/customers
+    */
     public static void postCustomer() throws Exception {
         HttpPost post = new HttpPost("http://localhost:9090/rest/customerservice/customers");
         StringEntity entity = new StringEntity("{\"Customer\":{\"id\":\"222\",\"name\":\"charles\"}}", "ISO-8859-1");
@@ -39,37 +43,11 @@ public class RestfullClient {
         }
     }
 
-    public static void getCustomer2() throws Exception {
-
-        /*
-            HTTPie req : http GET http://localhost:9090/rest/customerservice/customers/123
-            curl req : curl http://localhost:9090/rest/customerservice/customers/123
-         */
-
-        HttpGet get = new HttpGet("http://localhost:9090/rest/customerservice/customers/123");
-        // DOES NOT WORK
-        // get.addHeader("Accept" , "application/json");
-        get.addHeader("accept" , "*/*");
-        get.addHeader("content-type" , "application/json");
-        CloseableHttpClient httpclient = HttpClientBuilder.create().build();
-
-        try {
-            HttpResponse response = httpclient.execute(get);
-            int code = response.getStatusLine().getStatusCode();
-            String body = EntityUtils.toString(response.getEntity());
-            System.out.println("Status code : " + Integer.toString(code));
-            System.out.println("Response : " + body);
-        } finally {
-            httpclient.close();
-        }
-    }
-
+    /*
+      HTTPie req : http GET http://localhost:9090/rest/customerservice/customers/123
+      curl req : curl http://localhost:9090/rest/customerservice/customers/123
+    */
     public static void getCustomer() throws Exception {
-
-        /*
-            HTTPie req : http GET http://localhost:9090/rest/customerservice/customers/123
-            curl req : curl http://localhost:9090/rest/customerservice/customers/123
-         */
 
         HttpGet get = new HttpGet("http://localhost:9090/rest/customerservice/customers/123");
         get.addHeader("Accept" , "application/json");
