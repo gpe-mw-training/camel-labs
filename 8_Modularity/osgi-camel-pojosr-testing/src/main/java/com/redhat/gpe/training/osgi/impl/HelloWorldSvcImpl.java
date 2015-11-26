35078066ada6a4687d3b34ff0ca0136eff05a393
honghua chin <hchin@Docker-Swarm.local>
2015-11-26 23:13:36 +0800

    Renamed artifactID in project pom.xml files
    artifactID now contains same name as project folder names

--- 8_Modularity/osgi-camel-pojosr-testing/src/main/java/com/redhat/gpe/training/osgi/impl/HelloWorldSvcImpl.java
+++
@@ -1,9 +0,0 @@
-package com.redhat.gpe.training.osgi.impl;
-
-import com.redhat.gpe.training.osgi.HelloWorldSvc;
-
-public class HelloWorldSvcImpl implements HelloWorldSvc {
-    public String sayHello(String msg) {
-        return "Hello to " + msg + " user";
-    }
-}
