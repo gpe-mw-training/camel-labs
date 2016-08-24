5f703bc8ff3ea896a10c9d0197652034dfa59348
honghua chin <hchin@Docker-Swarm.local>
2016-08-24 22:10:37 +0800

    Last commit to start release of version 0.7 of camel-labs

--- 2_Core_Engine/src/main/java/com/redhat/gpe/training/camel/HelloBean.java
+++ 2_Core_Engine/src/main/java/com/redhat/gpe/training/camel/HelloBean.java
@@ -4,5 +4,5 @@ public class HelloBean {
     /*
      * Add a method to say Hello
      * Use as input the parameter the body received and concatenate it with "Hello World ! " text
-     */
+     */ 
 }
