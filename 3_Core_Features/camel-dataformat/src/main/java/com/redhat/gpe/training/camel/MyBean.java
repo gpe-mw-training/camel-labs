ee7389584fd2c2ce118ceb140f929c06a138e2f3
srinivasavasu <srinivasavasu@dhcp-0-130.blr.redhat.com>
2016-11-30 13:26:33 +0530

    BodyType updated from List to Student

--- 3_Core_Features/camel-dataformat/src/main/java/com/redhat/gpe/training/camel/MyBean.java
+++ 3_Core_Features/camel-dataformat/src/main/java/com/redhat/gpe/training/camel/MyBean.java
@@ -1,24 +1,16 @@
 package com.redhat.gpe.training.camel;
 
-import java.util.*;
+import java.util.Random;
 
 import org.apache.camel.Body;
+
 import com.redhat.gpe.training.camel.model.Student;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class MyBean {
 
-    private static final Logger logger = LoggerFactory.getLogger(MyBean.class);
-
-    public Student process(@Body List<?> list) throws Exception {
-        HashMap<String, Object> map = (HashMap<String, Object>) list.get(0);
-        String modelKey = Student.class.getName();
-        Student student = (Student) map.get(modelKey);
-
+    public Student process(@Body Student student) throws Exception {
         student.setStatus(Status.getRandomStatus().toString());
         student.setRoom("jboss-fuse-0123");
-
         return student;
     }
 
