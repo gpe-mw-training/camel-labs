bc133b4fbc8e727f0a2e32458982176a206b13ab
honghuac <honghuac@gmail.com>
2019-06-11 16:30:24 +0800

    https://github.com/redhat-gpe/fuse_on_openshift/issues/189

---
+++ 9_Camel-K/routes-rest.js
@@ -0,0 +1,64 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//
+// To run this integrations use:
+//
+//     kamel run --name=withrest --dependency=camel-undertow examples/routes-rest.js
+//
+
+// ****************
+//
+// Setup
+//
+// ****************
+
+l = components.get('log')
+l.exchangeFormatter = function(e) {
+    return "log - body=" + e.in.body + ", headers=" + e.in.headers
+}
+
+c = restConfiguration()
+c.component = 'undertow'
+c.port = 8080
+
+// ****************
+//
+// Functions
+//
+// ****************
+
+function proc(e) {
+    e.getIn().setHeader('RandomValue', Math.floor((Math.random() * 100) + 1))
+}
+
+// ****************
+//
+// Route
+//
+// ****************
+
+rest()
+    .path('/say/hello')
+    .produces("text/plain")
+    .get().route()
+        .transform().constant("Hello World");
+
+from('timer:js?period=1s')
+    .routeId('js')
+    .setBody()
+        .constant('Hello Camel K')
+    .process(proc)
+    .to('log:info')
